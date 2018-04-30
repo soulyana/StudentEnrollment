14a0bdd676b04c7c69fbdf61ef1900564f0c5624
soulyana <soulakew@umd.edu>
2018-04-30 17:00:54 -0400

    created add student on main controller

--- src/main/java/me/soulyana/demo/Controllers/MainController.java
+++ src/main/java/me/soulyana/demo/Controllers/MainController.java
@@ -1,9 +1,11 @@
 package me.soulyana.demo.Controllers;
 
+import me.soulyana.demo.Models.Student;
 import me.soulyana.demo.Repositories.CourseRepository;
 import me.soulyana.demo.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
@@ -14,5 +16,14 @@ public class MainController {
     @Autowired
     StudentRepository studentRepository;
 
+    public String showHomePage(Model model) {
+        model.addAttribute("menuoption", "homepage");
+        return "homepage";
+    }
 
+    public String addStudent(Model model) {
+        model.addAttribute("menuoption", "addstudent");
+        model.addAttribute("aStudent", new Student());
+        return "studentform";
+    }
 }
